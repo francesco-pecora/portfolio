0e644c6340f6c683b4d3906d1b7cf07b58771f00
francesco-pecora <francescogp9898@hotmail.com>
2019-12-04 23:44:29 -0500

    mysql database implemented

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from database_library import define_database, insert_query
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
+from database_library import define_database, insert_query
 
 @app.route('/')
 def home_route():
