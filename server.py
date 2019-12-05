f4955d29bc2ad7fd043f1535f5ee7fff54a031b1
francesco-pecora <francescogp9898@hotmail.com>
2019-12-04 23:37:11 -0500

    mysql database implemented

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-
+from database_library import define_database, insert_query
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
@@ -10,13 +10,9 @@ def home_route():
 def html_route(page_name):
     return render_template(page_name)
 
-# writing data to a txt file
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        database.write(f'\n\nEMAIL: {email},\nSUBJECT:  {subject},\nMESSAGE: {message}')
+def write_to_database(data):
+    db = define_database()
+    insert_query(db, str(data['email']), str(data['subject']), str(data['message']))
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
@@ -25,7 +21,7 @@ def submit_form():
             data = request.form.to_dict()
             # making sure we receive data
             if(data['email'] and data['subject'] and data['message']):
-                write_to_file(data)
+                write_to_database(data)
                 return redirect('thankyou.html')
             else:
                 return render_template("contact.html")
